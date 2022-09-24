6b99960708e231f6bf07a9a5d0fa6a5109ef1459
Daniel Chin <daniel.chin@nyu.edu>
2022-09-24 15:29:05 +0400

    define exp group display names

---
+++ SoundS3/linearity_shared.py
@@ -0,0 +1,7 @@
+EXP_GROUPS = [
+    ('VAE aug $\\times 4$, lock $z_\\mathrm{timbre}$', 'vae_symm_4_repeat'), 
+    ('VAE aug $\\times 4$, lock $z_\\mathrm{timbre}$ 10D', 'vae_symm_4_repeat_timbre10d'), 
+    (' AE aug $\\times 4$, lock $z_\\mathrm{timbre}$', 'ae_symm_4_repeat'), 
+    ('VAE aug $\\times 0$, lock $z_\\mathrm{timbre}$', 'vae_symm_0_repeat'), 
+    ('VAE aug $\\times 4$, RNN  $z_\\mathrm{timbre}$', 'vae_symm_4_no_repeat'), 
+]
