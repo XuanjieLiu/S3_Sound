43f605371678bb0ce3de914c86003f0f36b85c6a
Daniel Chin <daniel.chin@nyu.edu>
2022-09-24 17:04:49 +0400

    linearty z lattice plot first draft done

--- SoundS3/linearity_shared.py
+++ SoundS3/linearity_shared.py
@@ -1,11 +1,18 @@
 SPICE = 'SPICE'
 
+# EXP_GROUPS = [
+#     # display name, path name
+#     ('VAE aug $\\times 4$, lock $z_\\mathrm{timbre}$', 'vae_symm_4_repeat'), 
+#     ('VAE aug $\\times 4$, lock $z_\\mathrm{timbre}$ 10D', 'vae_symm_4_repeat_timbre10d'), 
+#     (' AE aug $\\times 4$, lock $z_\\mathrm{timbre}$', 'ae_symm_4_repeat'), 
+#     ('VAE aug $\\times 0$, lock $z_\\mathrm{timbre}$', 'vae_symm_0_repeat'), 
+#     ('VAE aug $\\times 4$, RNN  $z_\\mathrm{timbre}$', 'vae_symm_4_no_repeat'), 
+#     ('SPICE', SPICE), 
+# ]
+
 EXP_GROUPS = [
     # display name, path name
-    ('VAE aug $\\times 4$, lock $z_\\mathrm{timbre}$', 'vae_symm_4_repeat'), 
-    # ('VAE aug $\\times 4$, lock $z_\\mathrm{timbre}$ 10D', 'vae_symm_4_repeat_timbre10d'), 
-    # (' AE aug $\\times 4$, lock $z_\\mathrm{timbre}$', 'ae_symm_4_repeat'), 
-    ('VAE aug $\\times 0$, lock $z_\\mathrm{timbre}$', 'vae_symm_0_repeat'), 
-    # ('VAE aug $\\times 4$, RNN  $z_\\mathrm{timbre}$', 'vae_symm_4_no_repeat'), 
+    ('With Symmetry', 'vae_symm_4_repeat'), 
+    ('No Symmetry', 'vae_symm_0_repeat'), 
     ('SPICE', SPICE), 
 ]
--- SoundS3/plotZLattice.py
+++ SoundS3/plotZLattice.py
@@ -1,5 +1,21 @@
-import sys
 from os import path
+from typing import List, Dict
+
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
+from matplotlib.axes import Axes
+from matplotlib.figure import SubFigure
+from tqdm import tqdm
+
+plt.rcParams.update({
+    'text.usetex': True, 
+    'font.family': 'serif', 
+    'font.serif': ['Computer Modern'], 
+})
+
+FIGSIZE = (11, 5)
+WIDTH_RATIO = (7, 8)
+NECK_LINE_Y = .96
 
 from linearity_shared import *
 TASKS = [
@@ -39,30 +55,23 @@ COMMON_INSTRUMENTS = [
 
 SPICE_PATH = './SPICE_results/result_normalized.txt'
 
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.lines import Line2D
-from matplotlib.axes import Axes
-from tqdm import tqdm
-
-plt.rcParams.update({
-    'text.usetex': True, 
-    'font.family': 'Helvetica', 
-})
-
 def main():
-    fig = plt.figure(constrained_layout=True, figsize=(10, 5))
-    subfigs = fig.subfigures(1, 2, width_ratios=(2, 3))
-    for ((
+    fig = plt.figure(constrained_layout=True, figsize=FIGSIZE)
+    subfigs = fig.subfigures(1, 2, width_ratios=WIDTH_RATIO)
+    plotted: Dict[str, List[Line2D]] = {}
+    for subfig_i, ((
         task_path_name, task_display, 
         (x_path, x_display), 
         (y_path, y_display), 
         plt_style, 
-    ), subfig) in zip(TASKS, subfigs):
+    ), subfig) in enumerate(zip(TASKS, subfigs)):
+        subfig: SubFigure
         n_cols = len(EXP_GROUPS)
         if task_path_name == 'decode':
             n_cols -= 1 # for SPICE
-        axeses = subfig.subplots(len(DATA_SETS), n_cols)
+        axeses = subfig.subplots(
+            len(DATA_SETS), n_cols, sharex=False, sharey=True,
+        )
         for row_i, ((set_path, set_display), axes) in enumerate(zip(
             DATA_SETS, axeses, 
         )):
@@ -77,10 +86,10 @@ def main():
                         ax.axis('off')
                         continue
                     with open(SPICE_PATH, 'r') as f:
-                        for line in f:
-                            line: str = line.strip()
-                            line = line.split('single_note_GU_long/')[1]
-                            filename, z_pitch = line.split('.wav ')
+                        for neckLine in f:
+                            neckLine: str = neckLine.strip()
+                            neckLine = neckLine.split('single_note_GU_long/')[1]
+                            filename, z_pitch = neckLine.split('.wav ')
                             z_pitch = float(z_pitch)
                             instrument_name, pitch = filename.split('-')
                             pitch = int(pitch)
@@ -91,7 +100,7 @@ def main():
                                 X.append(pitch)
                                 Y.append(z_pitch)
                 else:
-                    result_path = RESULT_PATH % (task_path_name, set_path, exp_group[0])
+                    result_path = RESULT_PATH % (task_path_name, set_path, exp_group[1])
                     for instrument_name in COMMON_INSTRUMENTS:
                         if instrument_name not in COMMON_INSTRUMENTS:
                             continue
@@ -108,17 +117,44 @@ def main():
                         data[instrument_name] = (X, Y)
                 # plot X, Y
                 for instrument_name, (X, Y) in data.items():
-                    ax.plot(
+                    if instrument_name not in plotted:
+                        plotted[instrument_name] = []
+                    plotted[instrument_name].append(ax.plot(
                         X, Y, label=instrument_name, **plt_style, 
-                    )
-                if row_i == 0 or exp_group[0] is SPICE:
-                    ax.set_title(exp_group[1])
+                    )[0])
+                ax.set_title(' ')
+                if row_i == 0 or exp_group[1] is SPICE:
+                    ax.set_title(exp_group[0].replace(', ', '\n'))
                 if row_i == 1:
                     ax.set_xlabel(x_display)
                 if col_i == 0:
-                    ax.set_ylabel(set_display + '\n\n' + y_display)
+                    if y_path == 'z_pitch':
+                        kw = dict(rotation=0)
+                    else:
+                        kw = dict()
+                    ax.set_ylabel(y_display, **kw)
+                    if subfig_i == 0:
+                        ax.annotate(
+                            set_display, xy=(0, 0.5), 
+                            xytext=(-ax.yaxis.labelpad - 10, 0), 
+                            xycoords=ax.yaxis.label, 
+                            textcoords='offset points', 
+                            ha='right', va='center', 
+                            size='large',
+                        )
         subfig.suptitle(task_display)
-    plt.legend(markerscale=8)
+        neckLine = Line2D(
+            [0.02, .98], [NECK_LINE_Y], color='k', linewidth=1, 
+        )
+        subfig.add_artist(neckLine)
+    K, V = [], []
+    for k, v in plotted.items():
+        K.append(k)
+        V.append(v[0])
+    subfigs[1].legend(
+        V, K, markerscale=8, 
+        loc='upper right', bbox_to_anchor=(.97, .92), 
+    )
     # plt.tight_layout()
     plt.show()
 
